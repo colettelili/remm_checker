65a9ff5ad0c8f15978d6a0a95702361807dacd82
colettelili <iceputin@gmail.com>
2025-06-14 02:32:33 +0800

    Delete api directory

--- api/check.js
+++
@@ -1,8 +0,0 @@
-// api/check.js
-
-module.exports = async (req, res) => {
-  return res.status(200).json({
-    status: "ok",
-    time: new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" })
-  });
-};
